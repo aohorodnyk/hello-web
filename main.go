534e272fe748008d928d1f9c32d866a3b556802b
Anton Ohorodnyk <anton@ohorodnyk.name>
2020-05-30 11:53:39 -0700

    Add headers and get params

--- main.go
+++ main.go
@@ -25,8 +25,27 @@ func handlerMain(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 
 	w.Write([]byte(fmt.Sprintf("Hello, request was made to `%s`\n\n", r.RequestURI)))
-	w.Write([]byte("Environment variables:\n"))
 
+	getParams := r.URL.Query()
+	if len(getParams) > 0 {
+		w.Write([]byte("GET params:\n"))
+		for getName := range getParams {
+			getValue := r.URL.Query().Get(getName)
+			w.Write([]byte(fmt.Sprintf("\t%s: %s\n", getName, getValue)))
+		}
+
+		w.Write([]byte(fmt.Sprint("\n\n")))
+	}
+
+	w.Write([]byte("Headers:\n"))
+	for headerName := range r.Header {
+		headerValue := r.Header.Get(headerName)
+		w.Write([]byte(fmt.Sprintf("\t%s: %s\n", headerName, headerValue)))
+	}
+
+	w.Write([]byte(fmt.Sprint("\n\n")))
+
+	w.Write([]byte("Environment variables:\n"))
 	for _, env := range os.Environ() {
 		w.Write([]byte(fmt.Sprintf("\t%s\n", env)))
 	}
