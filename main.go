d63274cea91721c803f79b58a8aa15af15343ca7
Anton Ohorodnyk <anton@ohorodnyk.name>
2020-06-01 17:52:19 -0700

    Change w.Write to fmt.Fprintf

--- main.go
+++ main.go
@@ -24,29 +24,29 @@ func main() {
 func handlerMain(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 
-	w.Write([]byte(fmt.Sprintf("Hello, request was made to `%s`\n\n", r.RequestURI)))
+	fmt.Fprintf(w, "Hello, request was made to `%s`\n\n", r.RequestURI)
 
 	getParams := r.URL.Query()
 	if len(getParams) > 0 {
 		w.Write([]byte("GET params:\n"))
 		for getName := range getParams {
 			getValue := r.URL.Query().Get(getName)
-			w.Write([]byte(fmt.Sprintf("\t%s: %s\n", getName, getValue)))
+			fmt.Fprintf(w, "\t%s: %s\n", getName, getValue)
 		}
 
-		w.Write([]byte(fmt.Sprint("\n\n")))
+		fmt.Fprintf(w, "\n\n")
 	}
 
 	w.Write([]byte("Headers:\n"))
 	for headerName := range r.Header {
 		headerValue := r.Header.Get(headerName)
-		w.Write([]byte(fmt.Sprintf("\t%s: %s\n", headerName, headerValue)))
+		fmt.Fprintf(w, "\t%s: %s\n", headerName, headerValue)
 	}
 
 	w.Write([]byte(fmt.Sprint("\n\n")))
 
 	w.Write([]byte("Environment variables:\n"))
 	for _, env := range os.Environ() {
-		w.Write([]byte(fmt.Sprintf("\t%s\n", env)))
+		fmt.Fprintf(w, "\t%s\n", env)
 	}
 }
